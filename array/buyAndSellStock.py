a3e4043da7d7ab5f72a0a3e2a8f324acd3736263
yenaing-oo <64188092+yenaing-oo@users.noreply.github.com>
2024-04-19 20:07:17 -0500

    optimized time, now passes

--- array/buyAndSellStock.py
+++ array/buyAndSellStock.py
@@ -2,27 +2,16 @@
 # [3,2,6,5,0,3]
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxProfit = 0;
+        maxProfit = 0
         numDays = len(prices)
-        for startIdx in range(numDays):
-            profit = 0
-            myStock = prices[startIdx]
-            hasStock = True
-            for i in range(startIdx + 1, numDays):
-                priceToday = prices[i]
-                priceTmr = -1
-                if i+1 < numDays:
-                    priceTmr = prices[i+1]
-                if hasStock:
-                    if priceToday > myStock:
-                        profit += priceToday - myStock
-                        myStock = -1
-                        hasStock = False
-                
-                if(priceTmr > priceToday and not hasStock):
-                    myStock = priceToday
-                    hasStock = True
-
-            if profit > maxProfit:
-                maxProfit = profit
+        
+        for i in range(1, numDays):
+            # If the price today is higher than yesterday, we can sell for profit
+            if prices[i] > prices[i-1]:
+                maxProfit += prices[i] - prices[i-1]
+        
         return maxProfit
+    
+# Notes:
+# don't need to simulate real-life (i.e. don't overcomplicate it)
+# don't be afraid to use whatever shortcut is available to solve the problem
